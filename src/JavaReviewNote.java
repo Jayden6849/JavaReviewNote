3cffc0330b306a888804f095b53bba6f5b33ed14
Jayden <jayden6849@gmail.com>
2024-08-15 10:15:26 +0900

    Modift Review to JavaReviewNote

---
+++ src/JavaReviewNote.java
@@ -0,0 +1,10 @@
+public class JavaReviewNote {
+    public static void main(String[] args) {
+        /*
+
+        This is a repository for managing Java exercises
+        The purpose is to solve the problem in various ways
+
+         */
+    }
+}
